bcf2525d6c9bad1c571ee510a86e313c856cf204
00fanxi <90121162+00fanxi@users.noreply.github.com>
2022-02-05 14:46:03 +0900

    Add files via upload

---
+++ build/precache-manifest.c9d08a4a1520f08f4f8adbee6f8c150b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "672bf99d6b8c2144c041",
+    "url": "/static/js/main.672bf99d.chunk.js"
+  },
+  {
+    "revision": "9b204a5627261530ced1",
+    "url": "/static/js/1.9b204a56.chunk.js"
+  },
+  {
+    "revision": "672bf99d6b8c2144c041",
+    "url": "/static/css/main.94e27b2f.chunk.css"
+  },
+  {
+    "revision": "93f37c01d14171873d5302cea9e35409",
+    "url": "/index.html"
+  }
+];
